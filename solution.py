14a30677ec43e9058467aa9771774f9c96b18a43
AnastasiaBogacheva <130046692+AnastasiaBogacheva@users.noreply.github.com>
2023-04-07 23:24:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,6 +8,7 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     n = len(x)
     c = 2/(n*44*44)
-    z1 = gamma.ppf(alpha/2,n)
-    z2 = gamma.ppf(1-alpha/2,n)
+    l = np.sum(x)-n/2
+    z1 = gamma.ppf(alpha/2,n, loc = l)
+    z2 = gamma.ppf(1-alpha/2,n, loc = l)
     return (c*z1,c*z2)
